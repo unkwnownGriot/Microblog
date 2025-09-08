646889ab36900cae5bab8d0e82aabf8e14ba4413
Elsam Atchole <theo228boss@gmail.com>
2025-09-08 14:33:09 +0000

    update code

--- app/email.py
+++ app/email.py
@@ -6,7 +6,10 @@ from threading import Thread
 
 def send_async_mail(flask_app,msg):
     with flask_app.app_context():
-        mail.send(msg)
+        try:
+         mail.send(msg)
+        except Exception as e:
+           print(e)
 
 
 def send_mail(subject,sender,recipients,text_body,html_body):
